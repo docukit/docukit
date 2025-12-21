d66d031871fa8e2ee099e52f828318243c6c9618
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2025-12-21 21:24:35 +0000

    Revert "save decoupling docStore"
    This reverts commit e880952507fee0e301fa1ce6a9dc58126b12ff0d.

--- packages/docsync/src/exports/client.ts
+++ packages/docsync/src/exports/client.ts
@@ -1,3 +1,7 @@
-export { DocSyncClient, type ClientConfig } from "../client/index.js";
+export {
+  DocSyncClient,
+  type ClientConfig,
+  type GetDocArgs,
+} from "../client/index.js";
 export { IndexedDBProvider } from "../client/providers/indexeddb.js";
 export { type SerializedDoc } from "../shared/docBinding.js";
