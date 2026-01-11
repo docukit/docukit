e2168d3fb720ad3a1a408bb030df3c731a2d9e54
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2026-01-11 23:20:09 +0000

    add todo test providers

---
+++ tests/docsync/unit/providers/index.test.ts
@@ -0,0 +1,27 @@
+import { InMemoryServerProvider } from "@docnode/docsync/testing";
+import type { Provider } from "../../../../packages/docsync/dist/src/client/types.js";
+import { PostgresProvider } from "../../../../packages/docsync/dist/src/server/providers/postgres/index.js";
+import { IndexedDBProvider } from "../../../../packages/docsync/dist/src/client/providers/indexeddb.js";
+import { test } from "vitest";
+
+function testProvider<T extends "server" | "client", S, O>(
+  _type: T,
+  _provider: Provider<S, O, T>,
+) {
+  throw new Error("Not implemented");
+}
+
+test.todo("in memory provider", () => {
+  testProvider("server", new InMemoryServerProvider());
+});
+
+test.todo("postgres provider", () => {
+  testProvider("server", new PostgresProvider());
+});
+
+test.todo("indexeddb provider", () => {
+  testProvider(
+    "client",
+    new IndexedDBProvider({ userId: "test-user", secret: "test-secret" }),
+  );
+});
