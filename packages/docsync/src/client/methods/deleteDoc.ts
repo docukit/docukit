925c5521ded5e517c7b2c181ab7c01b9471a7208
German Jablonski <43938777+GermanJablo@users.noreply.github.com>
2026-02-20 22:49:42 +0000

    enhance deleteDocMethod to prevent re-deletion of already deleted documents and update cache accordingly; refactor import path for setupChangeListener

--- packages/docsync/src/client/methods/deleteDoc.ts
+++ packages/docsync/src/client/methods/deleteDoc.ts
@@ -9,6 +9,7 @@ export async function deleteDocMethod<
   const docId = args.docId;
   const cacheEntry = client["_docsCache"].get(docId);
   if (!cacheEntry) return;
+  if ((await cacheEntry.promisedDoc) === "deleted") return;
 
   const state = cacheEntry.localOpsBatchState;
   if (state) clearTimeout(state.timeout);
@@ -18,5 +19,9 @@ export async function deleteDocMethod<
   await local?.provider.transaction("readwrite", (ctx) =>
     ctx.saveOperations({ docId, operations: "deleted" }),
   );
+  client["_docsCache"].set(docId, {
+    ...cacheEntry,
+    promisedDoc: Promise.resolve("deleted"),
+  });
   void handleSync(client, docId);
 }
