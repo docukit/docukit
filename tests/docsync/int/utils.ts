132acc974d9d09565a025a8b797c2ede5cf3bbde
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2026-04-19 22:43:59 +0100

    Refactor IDB assertions in Local-First tests to remove clock parameter
    - Updated the `assertIDBDoc` method in `index.browser.test.ts` to eliminate the `clock` parameter, simplifying the assertions for document state.
    - Modified the `assertIDBDoc` method signature in `utils.ts` to reflect the removal of the `clock` parameter, enhancing type consistency.
    - Adjusted the `emptyIDB` export to align with the new method signature.
    
    These changes streamline the testing process and improve the clarity of IDB assertions.

--- tests/docsync/int/utils.ts
+++ tests/docsync/int/utils.ts
@@ -92,11 +92,7 @@ type ClientUtils = {
   loadDoc: () => Promise<void>;
   unLoadDoc: () => void;
   addChild: (text: string) => void;
-  assertIDBDoc: (expected?: {
-    clock: number;
-    doc: string[];
-    ops: string[];
-  }) => Promise<void>;
+  assertIDBDoc: (expected?: { doc: string[]; ops: string[] }) => Promise<void>;
   assertMemoryDoc: (children?: string[]) => Promise<void>;
   reqSpy: Mock<
     (
@@ -297,11 +293,7 @@ const createClientUtils = async (
       child.state.value.set(text);
       cachedDoc.root.append(child);
     },
-    assertIDBDoc: async (expected?: {
-      clock: number;
-      doc: string[];
-      ops: string[];
-    }) => {
+    assertIDBDoc: async (expected?: { doc: string[]; ops: string[] }) => {
       await expect
         .poll(async () => {
           if (!local) {
@@ -362,11 +354,9 @@ const createClientUtils = async (
             }
           }
 
-          expect({
-            clock: result.docResult.clock,
-            doc: actualDocChildren,
-            ops: opsChildren,
-          }).toStrictEqual(expected);
+          expect({ doc: actualDocChildren, ops: opsChildren }).toStrictEqual(
+            expected,
+          );
           return true;
         })
         .toBe(true);
@@ -402,4 +392,4 @@ const createClientUtils = async (
   };
 };
 
-export const emptyIDB = { clock: 0, doc: [], ops: [] };
+export const emptyIDB = { doc: [], ops: [] };
