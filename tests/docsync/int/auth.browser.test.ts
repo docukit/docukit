a0e46b2d0aa2014feea88ed6bf8fc39f76644517
Germán Jabloñski <43938777+GermanJablo@users.noreply.github.com>
2026-07-03 09:12:15 +0100

    save

--- tests/docsync/int/auth.browser.test.ts
+++ tests/docsync/int/auth.browser.test.ts
@@ -28,7 +28,7 @@ describe("Authentication", () => {
     const client = createClient("test-token-user1");
     const socket = client["_socket"];
     await vi.waitFor(() => expect(socket.connected).toBe(true), {
-      timeout: 500,
+      timeout: 1000,
     });
     socket.disconnect();
   });
